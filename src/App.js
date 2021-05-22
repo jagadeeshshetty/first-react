a9717930a7684001f2aa364955fcd039d98e315b
JagadeeshC <jagadeesh.cheluvaraj@gmail.com>
2021-05-22 11:01:13 +0530

    updated image

--- src/App.js
+++ src/App.js
@@ -9,13 +9,8 @@ function App() {
         <p>
           I'm <code>Jagadeesh C</code> 🙂
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React 💻
         </a>
       </header>
     </div>
