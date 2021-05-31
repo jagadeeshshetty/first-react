2c47d086cd04f951e106b9ec72648f556238f35c
JagadeeshC <jagadeesh.cheluvaraj@gmail.com>
2021-05-31 18:07:00 +0530

    12- Updating the State

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,13 +3,41 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ["tag1", "tag2", "tag3"],
+    // tags: [],
+  };
+
+  renderTag() {
+    if (this.state.tags.length === 0) return <p>There are no Tags.</p>;
+    return (
+      <ul>
+        {this.state.tags.map((tag) => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
+
+  /**
+   * Arrow function don't rebind 'this' keyword. It inherit it.
+   */
+  handleIncrement = () => {
+    this.setState({ count: this.state.count + 1 });
   };
 
   render() {
     return (
       <React.Fragment>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        <button
+          onClick={this.handleIncrement}
+          className="btn btn-secondary btn-sm"
+        >
+          Increment
+        </button>
+        <br></br>
+        {this.state.tags.length === 0 && "There are no Tags. Please create."}
+        {this.renderTag()}
       </React.Fragment>
     );
   }
